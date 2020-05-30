d7c3626f2c574f7e360d35ed5217fe9e5c9934dc
erosmerino <66147909+erosmerino@users.noreply.github.com>
2020-05-29 21:22:34 -0400

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,15 @@
+<?php
+$introducir_nombre = $_POST['introducir_nombre'];
+$introducir_email = $_POST['introducir_email'];
+$introducir_asunto = $_POST['introducir_asunto'];
+$introducir_mensaje = $_POST['introducir_mensaje'];
+
+$destinatario = "erosm3a@gmail.com";
+$asunto = "contacto sobre nuestra web";
+
+$carta = "de: $introducir_nombre \n";
+$carta .= "correo: $introducir_email \n";
+$carta .= "asunto: $introducir_asunto \n";
+$carta .= "mensaje: $introducir_mensaje";
+
+mail($destinatario, $asunto, $carta);
